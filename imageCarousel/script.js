fd3c7c8185bf0b588fa878d32c89235a13ae9157
Sam Gillies <sam@samgillies.com>
2023-04-03 17:30:41 +0100

    nav bar added, updates with clicks

--- imageCarousel/script.js
+++ imageCarousel/script.js
@@ -7,7 +7,7 @@ let slides = [  'https://source.unsplash.com/8H72wMWxj90',
 
 let slidesLength = slides.length - 1; 
 
-function createDOM(input) {
+function createDOM(input, nav) {
     let carousel = document.getElementById('carousel'); 
 
     let btnPrev = document.createElement('button');
@@ -22,6 +22,12 @@ function createDOM(input) {
     let slideDeck = document.createElement('div');
     slideDeck.setAttribute('class', 'slideDeck');
 
+    let slideNav = document.createElement('div');
+    slideNav.setAttribute('class', 'slideNav');
+    slideNav.setAttribute('id', 'slideNav');
+
+
+
     for (i = 0; i < input.length; i++) {
         let imageItem = document.createElement('div');
         imageItem.setAttribute('class', 'slide');
@@ -33,14 +39,22 @@ function createDOM(input) {
 
         imageItem.append(slideImage); 
         slideDeck.append(imageItem); 
+
+        let slideNavImage = document.createElement('button');
+        slideNavImage.setAttribute('id', `navbutton${i}`);
+        slideNavImage.setAttribute('class', `navBtn`);
+
+        slideNav.append(slideNavImage)
     }
 
-    carousel.append(btnPrev,btnNext,slideDeck);
+    carousel.append(btnPrev,btnNext,slideDeck,slideNav);
 }
 
 function eventListeners(i) {
     let btnPrev = document.getElementById('prev'); 
     let btnNext = document.getElementById('next'); 
+    let slideNav = document.getElementById('slideNav');
+
     let index = i; 
 
     let getSlide = document.getElementById(`slide${index}`);
@@ -73,6 +87,25 @@ function eventListeners(i) {
         let getSlideNew = document.getElementById(`slide${index}`);
         getSlideNew.classList.add('display'); 
     });
+
+    slideNav.addEventListener('click', (e) => {
+
+        getNavBtn = document.getElementById(`navbutton${index}`);
+        getNavBtn.classList.remove('navBtnSelected'); 
+
+        for (i = 0; i < slides.length; i++) {
+            if (e.target.id === `navbutton${i}`) {
+                getSlide = document.getElementById(`slide${index}`);
+                getSlide.classList.remove('display'); 
+                index = i; 
+                let getSlideNew = document.getElementById(`slide${index}`);
+                getSlideNew.classList.add('display'); 
+
+                e.target.classList.add('navBtnSelected');
+            }
+        }
+    }); 
+
 
 }
 
